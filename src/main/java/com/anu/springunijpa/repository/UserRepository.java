1410f90d915c47e2cc817f909865d778eb55642e
Anuththara99 <rajananda046@gmail.com>
2021-07-16 13:40:29 +0530

    add user repository

---
+++ src/main/java/com/anu/springunijpa/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.anu.springunijpa.repository;
+
+import com.anu.springunijpa.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository <User,Integer>{
+
+}
