72207b7bbe6b3d3a84346346645f17629c9f4e14
Anuththara99 <rajananda046@gmail.com>
2021-07-16 13:31:43 +0530

    User class created

---
+++ src/main/java/com/anu/springunijpa/entity/User.java
@@ -0,0 +1,65 @@
+package com.anu.springunijpa.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "User")
+public class User {
+    @Id
+    private int userId;
+    private String userName;
+    private Date dOB;
+    private String userTel;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public Date getdOB() {
+        return dOB;
+    }
+
+    public void setdOB(Date dOB) {
+        this.dOB = dOB;
+    }
+
+    public String getUserTel() {
+        return userTel;
+    }
+
+    public void setUserTel(String userTel) {
+        this.userTel = userTel;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", userName='" + userName + '\'' +
+                ", dOB=" + dOB +
+                ", userTel='" + userTel + '\'' +
+                '}';
+    }
+}
---
+++ src/test/java/com/anu/springunijpa/SpringUniJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anu.springunijpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUniJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
