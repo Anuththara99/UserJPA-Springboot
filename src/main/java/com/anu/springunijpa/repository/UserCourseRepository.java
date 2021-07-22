ac1be030519cc7536c1111aad973d31dc0d1b9a7
Anuththara99 <rajananda046@gmail.com>
2021-07-22 09:20:15 +0530

    delete usercourse entity,contrller, service & repository

--- src/main/java/com/anu/springunijpa/repository/UserCourseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.anu.springunijpa.repository;
-
-import com.anu.springunijpa.entity.UserCourse;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserCourseRepository extends JpaRepository<UserCourse, Long> {
-}
